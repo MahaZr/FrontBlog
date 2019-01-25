3ef85ea54fcfa0c3a744fe69413bdf5e470e4660
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-25 18:53:22 +0100

    struct

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ArticleComponent } from './article/article.component';
+import { NavbarComponent } from './shared/userInterfaces/navbar/navbar.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ArticleComponent,
+    NavbarComponent,
+    
   ],
   imports: [
     BrowserModule,
