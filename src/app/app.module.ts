98159a2a58a004f316c9ad195850fa66b4292e99
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-01-25 19:43:51 +0100

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-//import {Http} from '@angular/http';
+import {Http} from '@angular/http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
