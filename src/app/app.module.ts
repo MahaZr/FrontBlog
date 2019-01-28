3d9b7504b7279cd83bfdead56bf63e034ede5996
mahaZr <maha2074@hotmail.com>
2019-01-28 19:54:00 +0100

    service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import {Http} from '@angular/http';
+import {HttpModule} from '@angular/Http';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ArticleComponent } from './article/article.component';
 import { NavbarComponent } from './shared/userInterfaces/navbar/navbar.component';
+import { BlogService } from './shared/services/blog.service';
 
 
 @NgModule({
@@ -18,9 +19,9 @@ import { NavbarComponent } from './shared/userInterfaces/navbar/navbar.component
   imports: [
     BrowserModule,
     AppRoutingModule,
-    Http,
+    HttpModule
   ],
-  providers: [],
+  providers: [BlogService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
