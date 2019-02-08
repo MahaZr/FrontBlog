05789540edecf0da1717db7527728cd343456316
HAMMAMI Abdellaziz <abdellaziz.esti@gmail.com>
2019-02-08 18:56:48 +0100

    gestutilis

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,6 +9,7 @@ import { AuthGuard } from './shared/auth.guard';
 import { ArtbyautComponent } from './artbyaut/artbyaut.component';
 import { DashComponent } from './dash/dash.component';
 import { GestionarticleComponent } from './dash/gestionarticle/gestionarticle.component';
+import { GestionutilisateurComponent } from './dash/gestionutilisateur/gestionutilisateur.component';
 
 const routes: Routes = [
   {path:'',redirectTo:'/home',pathMatch:'full'},
@@ -19,7 +20,8 @@ const routes: Routes = [
   {path:'nav',component:NavbarComponent},
   {path:'home',component:HomeComponent},
   {path: 'dash', component:DashComponent, canActivate : [AuthGuard], children: [
-    {path :'gart', component:GestionarticleComponent}]},
+    {path :'gart', component:GestionarticleComponent},
+  {path:'guser',component:GestionutilisateurComponent}]},
   {path:"**",component:HomeComponent}
   /*{
      path: '', redirectTo: 'home'
--- src/app/dash/gestionutilisateur/gestionutilisateur.component.ts
+++ src/app/dash/gestionutilisateur/gestionutilisateur.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { GesUserService } from 'src/app/shared/services/ges-user.service';
+import { LoginService } from 'src/app/shared/services/login.service';
 
 @Component({
   selector: 'app-gestionutilisateur',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class GestionutilisateurComponent implements OnInit {
 
-  constructor() { }
+  constructor(private userServ : GesUserService, private auth : LoginService) { }
+
+ public users ;
+ public idAdmin;
 
   ngOnInit() {
+
+    
+this.getuserss();
+
+
+  }
+
+  getuserss(){
+
+    this.idAdmin = this.auth.getUser().user._id;
+
+    this.userServ.getUsers(this.idAdmin).subscribe(file => {
+      this.users = file;
+    })
+
+  }
+
+  addAdmin(userid){
+    console.log(this.idAdmin);
+
+    this.userServ.addAdmin(this.idAdmin,userid).subscribe(file =>{
+      console.log(file);
+      this.getuserss();
+    })
+  
+
+  }
+
+  deleteUser(userid){
+    this.userServ.deleteUser(this.idAdmin,userid).subscribe(file =>{
+      console.log(file);
+      this.getuserss();
+    })
+  
   }
 
 }
