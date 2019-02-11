61d7fb0ad046a74089487e499efe6e56c0acc733
mahaZr <maha2074@hotmail.com>
2019-02-11 20:48:08 +0100

    ajout art

--- src/app/dash/gestionarticle/creer-article/creer-article.component.ts
+++ src/app/dash/gestionarticle/creer-article/creer-article.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { GesArtService } from 'src/app/shared/services/ges-art.service';
+import { LoginService } from 'src/app/shared/services/login.service';
 
 @Component({
   selector: 'app-creer-article',
@@ -6,10 +9,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./creer-article.component.css']
 })
 export class CreerArticleComponent implements OnInit {
+  art: any = {};
+  public newArt;
+  public utilisateur
+  titre:string;
+  contenu:string;
 
-  constructor() { }
+
+
+  constructor(private rote: Router, private addArt: GesArtService, private keteb: LoginService) { }
 
   ngOnInit() {
+    // this.art.titre=this.titre;
+    // this.art.contenu=this.contenu,
+    this.utilisateur = this.keteb.getUser().user;
+    this.art.auteur = this.utilisateur.name + ' ' + this.utilisateur.lastename;
   }
 
+
+  ajArt() {
+    this.addArt.AjouterArts(this.art).subscribe(file => {
+      this.newArt = file;
+      console.log(file);
+      this.rote.navigate(['/dash']);
+    })
+  }
 }
